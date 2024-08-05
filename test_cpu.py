b3ce6e956079af5f400e705482b27540e2c28c9e
Neo-Liu66 <mortalneo@outlook.com>
2024-08-05 22:32:17 +0100

    tf不能用，只能用tf_runtime

--- test_cpu.py
+++ test_cpu.py
@@ -1,13 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Feb 29 13:47:54 2024
-
-@author: m29244lx
-"""
-
-# Autoencoder model quantization tests
-
-
 """
 #%%  import libraries
 import matplotlib.pyplot as plt
