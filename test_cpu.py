3c41a6c677f306242b2e742bcc470af36a4be693
Neo-Liu66 <mortalneo@outlook.com>
2024-08-06 17:04:27 +0100

    2D模型重建测试

--- test_cpu.py
+++ test_cpu.py
@@ -58,7 +58,7 @@ def runTFLite(input_data):
     results = []
     for sample in test_data:
         # Set input tensor
-        interpreter.set_tensor(input_details[0]['index'], sample.reshape((1, 800)))
+        interpreter.set_tensor(input_details[0]['index'], sample.reshape((1, 800,1)))
 
         # Run inference
         interpreter.invoke()
