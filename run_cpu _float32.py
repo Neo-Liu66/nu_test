420c0fa034112c7a346abc92db887d8ea77d14f1
Neo-Liu66 <mortalneo@outlook.com>
2024-08-14 07:30:38 +0100

    float32_CPU

--- run_cpu _float32.py
+++ run_cpu _float32.py
@@ -5,18 +5,13 @@ import time
 from tflite_runtime.interpreter import Interpreter
 
 script_dir = pathlib.Path(__file__).parent.absolute()
-model_file = os.path.join(script_dir, 'int8_cpu.tflite')
+model_file = os.path.join(script_dir, 'float32_cpu.tflite')
 print(f"测试点-模型路径：{model_file}")
 # model_file2 = os.path.join(script_dir, '2d_tpu.tflite')
 # print(f"测试点-模型路径：{model_file2}")
 data_file = os.path.join(script_dir, 'x_test_noisy1.npy')
 print(f"测试点-数据路径：{data_file}")
 data_file = np.load(data_file)
-
-#######################################################################
-# model_file='tpu_part.tflite'
-# 2d_cpu = np.load("x_test_noisy1.npy")
-########################################################################
 
 # %% 2. Run tensorflow lite models
 def runTFLite(input_data):
@@ -35,16 +30,11 @@ def runTFLite(input_data):
     print('获取输入输出信息成功')
     # Prepare the test dataset (replace with your test data)
 
-    #test_data = input_data.astype(np.float32)
-    min_val = np.min(input_data)
-    max_val = np.max(input_data)
-    scaled_data = (input_data - min_val) / (max_val - min_val) * 255 - 128
-    test_data = np.round(scaled_data).astype(np.int8)
 
     # Run inference on each test sample
     results = []
     start_time = time.time()
-    for sample in test_data:
+    for sample in data_file:
         # Set input tensor
         interpreter.set_tensor(input_details[0]['index'], sample.reshape((1,1, 800,1)))
         # Run inference
@@ -59,15 +49,13 @@ def runTFLite(input_data):
     results = np.array(results)
     print(results.shape)
     results = np.squeeze(results, axis=(1, 2, 4))
-    results = (results + 128) / 255 * (max_val - min_val) + min_val
     return results, total_time
 
 
 def main():
     decoded_layer, total_time = runTFLite(data_file)
-    print(f'Int 8 on CPU Inference time is:{total_time}')
-    np.save('int8_CPU_result.npy', decoded_layer)
-    print("First 5 samples of quantized data:", decoded_layer[:5])
+    print(f'Float 32 on CPU Inference time is:{total_time}')
+    np.save('float32_CPU_result.npy', decoded_layer)
 
 if __name__ == "__main__":
     main()
