f65aa4f11475d3771f0bb8678433151e44e76131
Neo-Liu66 <mortalneo@outlook.com>
2024-08-17 12:42:56 +0100

    尝试原模型_cpu

--- run_cpu_test.py
+++ run_cpu_test.py
@@ -5,14 +5,14 @@ import time
 from tflite_runtime.interpreter import Interpreter
 
 script_dir = pathlib.Path(__file__).parent.absolute()
-model_file = os.path.join(script_dir, 'cpu_test2.tflite')
+model_file = os.path.join(script_dir, 'cpu_test_ori.tflite')
 print(f"测试点-模型路径：{model_file}")
 # model_file2 = os.path.join(script_dir, '2d_tpu.tflite')
 # print(f"测试点-模型路径：{model_file2}")
 data_file = os.path.join(script_dir, 'x_test_noisy1.npy')
 print(f"测试点-数据路径：{data_file}")
-data_file = np.load(data_file)
-
+input_data = np.load(data_file)
+print(f"测试点-打印数据：{input_data}")
 
 # %% 2. Run tensorflow lite models
 def runTFLite(input_data):
@@ -30,12 +30,12 @@ def runTFLite(input_data):
     output_details = interpreter.get_output_details()
     print('获取输入输出信息成功')
     # Prepare the test dataset (replace with your test data)
-    test_data = input_data
+    data_file = input_data.astype(np.float32)
 
     # Run inference on each test sample
     results = []
     start_time = time.time()
-    for sample in test_data:
+    for sample in data_file:
         # Set input tensor
         interpreter.set_tensor(input_details[0]['index'], sample.reshape((1,1, 800,1)))
         # Run inference
@@ -54,13 +54,14 @@ def runTFLite(input_data):
 
 
 def main():
-    decoded_layer, total_time = runTFLite(data_file)
-    print(f'float 32 on CPU Inference time is:{total_time}')
-    np.save('float 32_CPU_result.npy', decoded_layer)
-    print("First 5 samples of quantized data:", decoded_layer[:5])
+    decoded_layer, total_time = runTFLite(input_data)
+    print(f'Float 32 on CPU Inference time is:{total_time}')
+    np.save('float32_CPU_result.npy', decoded_layer)
 
 if __name__ == "__main__":
     main()
+
+
 
 
 '''
